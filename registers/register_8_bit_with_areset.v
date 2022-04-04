4b568c31ff74d7b114ac9614e0131c7f60c9dc00
Paweł Augustyniak <81176417+guustek@users.noreply.github.com>
2022-04-04 03:12:25 +0200

    .

--- registers/register_8_bit_with_areset.v
+++
@@ -1,9 +0,0 @@
-module register_8_bit_with_areset(
-	input clk, reset,
-	input wire [7:0] data,
-	output reg [7:0] out);
-	
-	always @(posedge clk, posedge reset)
-		if (reset) out <= 0;
-		else out <= data;
-endmodule
